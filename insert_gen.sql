84a7794b1c4b970a923ba78ac8df16afa8b7b782
Oh Seyeong <manusil@nate.com>
2024-04-03 15:37:42 +0900

    라인 조절

--- insert_gen.sql
+++ insert_gen.sql
@@ -18,60 +18,29 @@
                      , REGISTER_ID
                      , MODF_DT
                      , MODFER_ID ) VALUES ( ''%s'', ''%s'', %s,  ''%s'', ''%s'', ''%s'', ''%s'',  ''%s'',  ''%s'', %s, ''%s'', ''%s'',  ''%s'', ''%s'', ''%s'', ''%s'', ''%s'',  ''%s'' );'
-                     ,
-	'LAY_000016'
-                     ,
-	'1' 
-                     ,
-	row_number() over ()
-                     ,
-	c.column_name
+                     , 'LAY_000016'
+                     , '1' 
+                     , row_number() over () , c.column_name
                      
-                     ,
-	coalesce(col_description(pg_class.oid,
-	c.ordinal_position::int),
-	'NULL')
-                     ,
-	''
-                     ,
-	c.data_type 
-                     ,
-	coalesce(cast(c.character_maximum_length as text),
-	'NULL')       
+                     , coalesce(col_description(pg_class.oid, c.ordinal_position::int), 'NULL')
+                     , ''
+                     , c.data_type 
+                     , coalesce(cast(c.character_maximum_length as text), 'NULL')       
                      
-                     ,
-	coalesce(cast(c.numeric_scale as text),
-	'NULL')
-                     ,
-	row_number() over ()
-                     ,
-	'N'
-                     ,
-	case
-		when c.is_nullable = 'YES' then 'Y'
-		else 'N'
-	end
-                     ,
-	null    
+                     , coalesce(cast(c.numeric_scale as text), 'NULL')
+                     , row_number() over ()
+                     , 'N'
+                     , case when c.is_nullable = 'YES' then 'Y' else 'N' end
+                     , null    
 
-                     ,
-	''
-                     ,
-	CURRENT_TIMESTAMP
-                     ,
-	'admin'
-                     ,
-	CURRENT_TIMESTAMP
-                     ,
-	'admin'
-                     
+                     , ''
+                     , CURRENT_TIMESTAMP 
+                     , 'admin'
+                     , CURRENT_TIMESTAMP
+                     , 'admin'
                      )
-from
-	information_schema.columns c
-join pg_class on
-	c.table_name = pg_class.relname
-join pg_namespace on
-	pg_namespace.oid = pg_class.relnamespace
+from information_schema.columns c
+join pg_class on c.table_name = pg_class.relname
+join pg_namespace on pg_namespace.oid = pg_class.relnamespace
 	and c.table_schema = pg_namespace.nspname
-where
-	c.table_name = 'tit_layout_column';
+where c.table_name = 'tit_layout_column';
